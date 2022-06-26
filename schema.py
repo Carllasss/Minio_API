7d2cd7e70700f20ae6d0f9eaea7e1715a03fc2d1
Carllass <Evilangel3211@gmail.com>
2022-06-26 19:26:47 +0500

    added date folders in minio

--- schema.py
+++ schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 
-class Inbox(BaseModel):
+class GetFrame(BaseModel):
     title: str
     created_at: str
--- services/frame_service.py
+++ services/frame_service.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import HTTPException
 from uuid import uuid4
 
@@ -12,11 +14,12 @@ db_client = DbClient()
 def post(files):
     request_id = uuid4()
     new_files_names = []
+    date = datetime.now().strftime('%Y.%m.%d')
     for file in files:
         try:
             file_name = (str(uuid4()) + '.jpg')
             new_files_names.append(file_name)
-            minio_client.minio_post(file_name, file)
+            minio_client.minio_post(file_name, file, date)
             db_client.db_post(file_name, request_id)
 
         except Exception:
@@ -37,10 +40,11 @@ def get(request_id):
 
 def delete(request_id):
     frames = db_client.db_get_by_request_id(request_id).all()
-    data = [{'id': frame.id} for frame in frames]
+    data = [{'created_at': frame.created_at} for frame in frames]
     if data == []:
         raise HTTPException(status_code=404, detail="Frame not found")
+    date = (data[0]['created_at']).strftime('%Y.%m.%d')
     for frame in frames:
-        minio_client.minio_delete(frame)
+        minio_client.minio_delete(frame, date)
         db_client.db_delete(frame)
     return 204
